dfa9698d0b1052d259c50dca2a178425279224bf
Gustavo Kissel <kisselgustavo@gmail.com>
2023-09-19 16:47:35 -0300

    Formatter Use on summary and Transactions History

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dataFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
 
@@ -22,9 +23,12 @@ export function Transactions() {
                     return ( 
                     <tr key={transaction.id}>
                         <td width='50%'>{transaction.description}</td>
-                        <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+                        <td><PriceHighlight variant={transaction.type}>
+                                {transaction.type === 'outcome' && '- '}
+                                {priceFormatter.format(transaction.price)}
+                            </PriceHighlight></td>
                         <td>{transaction.category}</td>
-                        <td>{transaction.createdAt}</td>
+                        <td>{dataFormatter.format(new Date(transaction.createdAt))}</td>
                     </tr>)
                 })}
                 </tbody>
