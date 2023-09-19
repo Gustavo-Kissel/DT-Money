eb5e070630e97037cb01cbe8265725c9cfc56ee2
Gustavo Kissel <kisselgustavo@gmail.com>
2023-09-19 11:23:16 -0300

    Radix-UI modal creation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,29 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/Ignite-Logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt=""></img>
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Novo Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                           <Dialog.Title>Nova Transação</Dialog.Title>
+                           <Dialog.Close />
+                           {/* <Dialog.Body>
+                             <NewTransactionForm />
+                           </Dialog.Body> */}
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
