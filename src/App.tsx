7e0789ca7c5bbc42e0974795c520637a4c8f7667
Gustavo Kissel <kisselgustavo@gmail.com>
2023-09-19 08:41:30 -0300

    Theme Provider Added on App/

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   
 
   return (
-    <>
-      
+    <ThemeProvider theme={defaultTheme}>
+    <> 
     </>
+    </ThemeProvider>
   )
 }
 
