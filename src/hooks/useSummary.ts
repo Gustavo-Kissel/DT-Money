7dd93dc1db733aafcc9e1ac5c4143e6a975aef24
Gustavo Kissel <kisselgustavo@gmail.com>
2023-09-21 14:51:06 -0300

    Remove useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -12,26 +12,24 @@ export function useSummary() {
     },
   )
 
-  const summary = useMemo(() => {
-    transactions.reduce(
-      (acc, transaction) => {
-        if (transaction.type === 'income') {
-          acc.income += transaction.price
-          acc.total += transaction.price
-        } else if (transaction.type === 'outcome') {
-          acc.outcome += transaction.price
-          acc.total -= transaction.price
-        }
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else if (transaction.type === 'outcome') {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
 
-        return acc
-      },
-      {
-        income: 0,
-        outcome: 0,
-        total: 0,
-      },
-    )
-  }, [transactions])
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
 
   return summary
 }
